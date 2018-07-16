e58fc58f7bd5fb86531015f7e7cfa3b8239d1369
Danihcv <dhcv@ic.ufal.br>
2018-07-15 23:06:47 -0300

    Interface gráfica finalizada; Funções iniciais implementadas.

---
+++ functions.js
@@ -0,0 +1,104 @@
+var equationStr;
+var xCoefficient;
+var steps = [];
+
+class Node {
+    constructor(op, a, b) {
+        this.op = op;
+        this.a = a;
+        this.b = b;
+    }
+}
+
+function submit() {
+    steps.length = 0;
+
+    var stepsContainer = document.getElementById('stepsContainer');
+    stepsContainer.innerHTML = '';
+
+    removeSpaces();
+    steps.push(prepareStep(equationStr));
+
+    var thirdStep = passElementsToSecondMember();
+    var fourthStep = new Node('-', new Node('-', new Node('+', 4, 3), 5), 9);
+    var fifthStep = solveOperationsTree(fourthStep);
+    console.log('res', fifthStep);
+
+    console.log('ss', steps);
+    steps.forEach((step) => {
+        stepsContainer.innerHTML += '<div class=\"step\">' + step + '</div>';
+    });
+}
+
+function removeSpaces() {
+    equationStr = document.getElementById('equation').value;
+    equationStr = equationStr.replace(/ /g, '');
+    console.log('eqStr:', equationStr);
+}
+
+function prepareStep(eqStr) {
+    var elements = eqStr.split(/([+\-=])|([0-9]+)/g);
+    var step = '';
+
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i]) {
+            if (i > 0) {
+                step += ' ';
+            }
+
+            step += elements[i];
+        }
+    }
+
+    return step;
+}
+
+function passElementsToSecondMember() {
+    var frstMember = equationStr.split('=')[0];
+    var scndMember = equationStr.split('=')[1];
+    var swapMember = '';
+
+    var auxNum = '';
+    for (var i = frstMember.length - 1; i >= 0; i--) {
+        var intOrNan = Number(frstMember[i]);
+
+        if (!isNaN(intOrNan)) {
+            auxNum = intOrNan + auxNum;
+
+            if (i === 0) {
+                swapMember = '-' + Number(auxNum) + swapMember;
+                auxNum = '';
+            }
+        } else {
+            var op = frstMember[i];
+
+            auxNum = (op === '+' ? -1 : 1) * Number(auxNum);
+            swapMember = (op === '+' ? '' : '+') + auxNum + swapMember;
+            auxNum = '';
+        }
+    }
+    console.log('scndMember', scndMember + swapMember);
+    return scndMember + swapMember;
+}
+
+function solveOperationsTree(node) {
+    if (isLeaf(node)) {
+        return node;
+    }
+
+    var res;
+    switch (node.op) {
+        case '+':
+            res = solveOperationsTree(node.a) + solveOperationsTree(node.b);
+            break;
+        case '-':
+            res = solveOperationsTree(node.a) - solveOperationsTree(node.b);
+            break;
+    }
+
+    return res;
+}
+
+function isLeaf(node) {
+    return node.a === undefined && node.b === undefined;
+}
